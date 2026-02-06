9507839f8ac3687d986992eff7a32929dc8c83a6
Rohit-intg <rohit.kalia@intelegencia.com>
2026-02-06 17:39:57 +0530

    feat: add user signup and login schemas

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from schemas.auth import SignupRequest , LoginRequest
+
 
 app = FastAPI(title="Daily Growth OS")
+
+
+
 
 @app.get("/")
 def health_check():
     return {"status": "Backend running"}
+
+
