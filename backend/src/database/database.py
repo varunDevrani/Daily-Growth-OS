506c7e2b493b089c6b1b9a5035d5fe5fa42874e9
Rohit-intg <rohit.kalia@intelegencia.com>
2026-02-10 12:46:21 +0530

    chore: add dotenv setup for environment variables

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,8 +1,12 @@
-import os
+import os 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
@@ -13,3 +17,4 @@ def get_db():
         yield db
     finally:
         db.close()
+        
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from src.routes.auth import router as auth_router
+from src.database.base import Base
+from src.database.database import engine
 
 app = FastAPI(title="Daily Growth OS")
 
-
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def health_check():
     return {"status": "Backend running"}
-
-app.include_router(auth_router, prefix="/api/v1")
