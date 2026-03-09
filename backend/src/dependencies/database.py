ce471475ea981e4c9ddaeef53e0f62cba241c1bf
varunDevrani <varun.devrani@intelegencia.com>
2026-03-09 19:57:20 +0530

    fix import error of sessionLocal

--- backend/src/dependencies/database.py
+++ backend/src/dependencies/database.py
@@ -1,7 +1,7 @@
-from src.database.database import SessionLocal
+from src.database.database import sessionLocal
 
 def get_db():
-	db = SessionLocal()
+	db = sessionLocal()
 	try:
 		yield db
 		db.commit()
