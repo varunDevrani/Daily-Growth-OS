4392c784cc8f1bc2992934255abc75bd54d8f3f0
varunDevrani <varun.devrani@intelegencia.com>
2026-03-09 17:49:23 +0530

    add UserResponse and SuccessResponse

---
+++ backend/src/schemas/user.py
@@ -0,0 +1,16 @@
+from typing import Union
+from uuid import UUID
+
+from pydantic import ConfigDict
+
+from src.schemas.base import BaseSchema
+
+
+class UserResponse(BaseSchema):
+	model_config = ConfigDict(from_attributes=True)
+
+	id: UUID
+	first_name: Union[str, None] = None
+	last_name: Union[str, None] = None
+	email: str
+	profile_pic_url: Union[str, None] = None
