329d0dfa997594d7b81edbaeed5a69f6a922847d
varunDevrani <varun.devrani@intelegencia.com>
2026-03-13 13:56:19 +0530

    add auth routes for logout and token refresh

--- backend/src/controllers/auth.py
+++ backend/src/controllers/auth.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 import src.services.auth as services
 from src.schemas.api_response import SuccessResponse
-from src.schemas.auth import LoginRequest, SignupRequest, TokenResponse
+from src.schemas.auth import LoginRequest, RefreshTokenRequest, SignupRequest, TokenResponse
 from src.schemas.user import UserResponse
 
 
@@ -34,3 +34,34 @@ def login(
 		message="User logged in.",
 		data=token_data
 	)
+
+
+def logout(
+	payload: RefreshTokenRequest,
+	db: Session
+) -> SuccessResponse:
+	_ = services.logout(
+		payload,
+		db
+	)
+	
+	return SuccessResponse(
+		message="user successfully logout"
+	)
+
+
+def refresh(
+	payload: RefreshTokenRequest,
+	db: Session
+) -> SuccessResponse[TokenResponse]:
+	token_data = services.refresh(
+		payload,
+		db
+	)
+	
+	return SuccessResponse[TokenResponse](
+		message="new access token generated for user",
+		data=token_data
+	)
+	
+
